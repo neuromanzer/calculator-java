0bd66c6a0ff1d9251c5746fb2e2bcda83f3adb31
neuromanzer <krokusfs@yahoo.com>
2020-02-15 21:05:22 +0300

    add input arguments count condition

--- src/Main.java
+++ src/Main.java
@@ -7,13 +7,17 @@ public class Main {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         System.out.print("Enter: ");
         String[] inputArray = reader.readLine().split("\\s+");
-        String sign = inputArray[1];
-        String strA = inputArray[0];
-        String strB = inputArray[2];
+        if (inputArray.length == 3) {
+            String strA = inputArray[0];
+            String sign = inputArray[1];
+            String strB = inputArray[2];
 
-        Calculator calculator = new Calculator();
-        calculator.parse(strA, strB);
-        String result = calculator.calculateToString(sign);
-        System.out.println(result);
+            Calculator calculator = new Calculator();
+            calculator.parse(strA, strB);
+            String result = calculator.calculateToString(sign);
+            System.out.println(result);
+        } else {
+            throw new IllegalArgumentException("Wrong argument count.");
+        }
     }
 }
